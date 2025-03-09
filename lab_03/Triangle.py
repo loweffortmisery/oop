1b63cd390d17239c2ea619bca1400a9fcde4f1c8
loweffortmisery <189833560+loweffortmisery@users.noreply.github.com>
2025-03-09 20:33:51 +0200

    added Triangle.py in lab_03

--- lab_03/Triangle.py
+++ lab_03/Triangle.py
@@ -1,12 +1,25 @@
-class Triangle:
+class Triangle: 
     def __init__(self, a, b, c):
         assert (a + b > c and a + c > b and b + c > a), "Неможливо утворити трикутник з такими сторонами"
         self.a = a
         self.b = b
         self.c = c
     
-    def Perimeter(self):
+    def __str__(self):
+        return f"Triangle: {self.__position, self.__vertex1, self.__vertex2}"
+ 
+    def perimeter(self):
         return self.a + self.b + self.c
+    
+    def area(self):
+        p = (self.a + self.b + self.c)/2
+        p_a = p - self.a
+        p_b = p - self.b
+        p_c = p - self.c
+        return (p * p_a * p_b * p_c)**0.5
+
+
+
 
 
 
