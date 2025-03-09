062bdccc7e8cc71cb30cc1fd2489bd51cb04476e
loweffortmisery <189833560+loweffortmisery@users.noreply.github.com>
2025-03-09 20:34:07 +0200

    added Rectangle.py in lab_03

---
+++ lab_03/Rectangle.py
@@ -0,0 +1,14 @@
+class Rectangle:
+    def __init__(self, a, b){
+        assert (a > 0 and b > 0), "Неможливо утворити прямокутник з такими сторонами"
+        self.a = a
+        self.b = b
+    }
+
+    def perimeter(self):
+        return 2 * (self.a + self.b)
+    def area(self):
+        return self.a * self.b
+
+    def __str__(self):
+        return f"Прямокутник з сторонами {self.a} і {self.b}"
